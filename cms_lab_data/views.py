a44d17ac2d158fded45de2b6826ae4ad98796952
Mike Covington <mfcovington@gmail.com>
2015-06-19 14:33:35 -0700

    Create generic Detail/List views for DataFile and DataFileSet

--- cms_lab_data/views.py
+++ cms_lab_data/views.py
@@ -1,3 +1,22 @@
-from django.shortcuts import render
+from django.views.generic import DetailView, ListView
 
-# Create your views here.
+from .models import DataFile, DataFileSet
+
+class DataFileDetailView(DetailView):
+
+    model = DataFile
+
+
+class DataFileListView(ListView):
+
+    model = DataFile
+
+
+class DataFileSetDetailView(DetailView):
+
+    model = DataFileSet
+
+
+class DataFileSetListView(ListView):
+
+    model = DataFileSet
