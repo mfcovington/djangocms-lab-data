75e823cea5ce69f20e4a079cca0c6b032482e16d
Mike Covington <mfcovington@gmail.com>
2015-06-20 02:21:13 -0700

    Create data_view and template for front page of data

--- cms_lab_data/urls.py
+++ cms_lab_data/urls.py
@@ -1,9 +1,10 @@
 from django.conf.urls import url
 
-from .views import (DataFileDetailView, DataFileListView,
+from .views import (data_view, DataFileDetailView, DataFileListView,
     DataFileSetDetailView, DataFileSetListView)
 
 urlpatterns = [
+    url(r'^$', data_view, name='data_view'),
     url(r'^files/$', DataFileListView.as_view(), name='data_file_list'),
     url(r'^files/(?P<slug>[-\w]+)/$', DataFileDetailView.as_view(), name='data_file_detail'),
     url(r'^sets/$', DataFileSetListView.as_view(), name='data_file_set_list'),
--- cms_lab_data/views.py
+++ cms_lab_data/views.py
@@ -1,6 +1,15 @@
+from django.shortcuts import render
 from django.views.generic import DetailView, ListView
 
 from .models import DataFile, DataFileSet
+
+def data_view(request):
+    context = {
+        'data_file_count': DataFile.objects.count(),
+        'data_file_set_count': DataFileSet.objects.count(),
+    }
+    return render(request, 'cms_lab_data/data.html', context)
+
 
 class DataFileDetailView(DetailView):
 
