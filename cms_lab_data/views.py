c9dc7958e84fa838a29b8e10aae1d0be953a58ff
Mike Covington <mfcovington@gmail.com>
2015-06-21 23:04:37 -0700

    Add to searchable data file list to DataFileSetDetailView

--- cms_lab_data/views.py
+++ cms_lab_data/views.py
@@ -46,6 +46,28 @@ class DataFileSetDetailView(DetailView):
 
     model = DataFileSet
 
+    def get_context_data(self, **kwargs):
+        context = super(DataFileSetDetailView, self).get_context_data(**kwargs)
+
+        data_file_set_tags = {}
+        data_file_set = self.object.data_files.all()
+        for data_file in data_file_set:
+            for tag in data_file.tags.all():
+                data_file_set_tags[tag.id] = tag.name
+
+        data_file_set_tags = collections.OrderedDict(
+            sorted(data_file_set_tags.items(), key=operator.itemgetter(1)))
+
+
+        context['data_file_set'] = data_file_set
+        context['data_file_set_tags'] = data_file_set_tags
+        context['label'] = self.object.label
+        context['pagination'] = 10
+        context['searchable'] = True
+        context['max_words'] = 40
+        context['word_buffer'] = 10
+        return context
+
 
 class DataFileSetListView(ListView):
 
