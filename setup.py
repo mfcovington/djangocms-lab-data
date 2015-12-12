5a68e9bd9e3907e84d490ca1ef3128b25fcf4437
Mike Covington <mfcovington@gmail.com>
2015-12-11 22:32:25 -0800

    Convert README to reStructuredText format

--- setup.py
+++ setup.py
@@ -6,8 +6,11 @@ if sys.version_info < (3, 2):
     print("Sorry, djangocms-lab-data currently requires Python 3.2+.")
     sys.exit(1)
 
-with open(os.path.join(os.path.dirname(__file__), 'README.md')) as readme:
-    README = readme.read()
+# From: https://hynek.me/articles/sharing-your-labor-of-love-pypi-quick-and-dirty/
+def read(*paths):
+    """Build a file path from *paths* and return the contents."""
+    with open(os.path.join(*paths), 'r') as f:
+        return f.read()
 
 # allow setup.py to be run from any path
 os.chdir(os.path.normpath(os.path.join(os.path.abspath(__file__), os.pardir)))
@@ -27,7 +30,7 @@ setup(
     include_package_data=True,
     license='BSD License',
     description='A Django app for organizing data files and sets of data files within a Django site with django CMS-specific features',
-    long_description=README,
+    long_description=(read('README.rst'))
     url='https://github.com/mfcovington/djangocms-lab-data',
     author='Michael F. Covington',
     author_email='mfcovington@gmail.com',
